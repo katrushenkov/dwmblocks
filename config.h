91ed7f88ff7150288f7d4d1367e5b0c9da16360e
Sergey Katrushenkov <katrushenkov@gmail.com>
2020-11-17 18:45:00 +1000

    some changes

--- config.h
+++ config.h
@@ -3,28 +3,33 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	/* {"",	"music",	0,	11},*/
-	{"",	"pacpackages",	0,	8},
-	{"",	"news",		0,	6},
+	/* {"",	"pacpackages",	0,	8}, */
+	/* {"",	"news",		0,	6}, */
 	/* {"",	"crypto",	0,	13}, */
 	/* {"",	"price bat \"Basic Attention Token\" 🦁",	0,	20}, */
 	/* {"",	"price btc Bitcoin 💰",				0,	21}, */
 	/* {"",	"price lbc \"LBRY Token\" 📚",			0,	22}, */
-	{"",	"torrent",	20,	7},
-	/* {"",	"memory",	10,	14}, */
+	/* {"",	"torrent",	20,	7},  */
+	{"",	"memory",	10,	14},
 	/* {"",	"cpu",		10,	18}, */
 	/* {"",	"moonphase",	18000,	17}, */
-	{"",	"weather",	18000,	5},
-	{"",	"mailbox",	180,	12},
+	/* {"",	"weather",	18000,	5},  */
+	/* {"",	"mailbox",	180,	12}, */
 	/* {"",	"nettraf",	1,	16}, */
+	{"",	"microweather",	10,	13},
+	//{"",	"teststatus",	0,	14},
 	{"",	"volume",	0,	10},
+	{"",	"lglight",	0,	11},
 	{"",	"battery",	5,	3},
 	{"",	"clock",	60,	1},
+//	{"",	"hackernews",	0,	16},
+	{"",	"layout",	0,	12},
 	{"",	"internet",	5,	4},
 	{"",	"help-icon",	0,	15},
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char *delim = " ";
+static char *delim = " │ ";
 
 // Have dwmblocks automatically recompile and run when you edit this file in
 // vim with the following line in your vimrc/init.vim:
