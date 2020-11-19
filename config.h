9852ecb1d79b3603beb10589d5283daa86b7cc05
Sergey Katrushenkov <katrushenkov@gmail.com>
2020-11-19 19:30:37 +1000

    little clean of config.h

--- config.h
+++ config.h
@@ -29,11 +29,5 @@ static const Block blocks[] = {
 	{"",	"help-icon",	0,	15},
 };
 
-
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
 static char *delim = " │ ";
-
-// Have dwmblocks automatically recompile and run when you edit this file in
-// vim with the following line in your vimrc/init.vim:
-
-// autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
