a3bdc50884c7dce7377bc62dbe3099ecc2f5c010
Sergey Katrushenkov <katrushenkov@gmail.com>
2021-01-06 21:50:08 +1000

    changes

--- config.h
+++ config.h
@@ -4,7 +4,7 @@ static const Block blocks[] = {
 	/* {"⌨", "sb-kbselect", 0, 30}, */
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	/* {"",	"sb-music",	0,	11},*/
-	/* {"",	"sb-pacpackages",	0,	8}, */
+	{"",	"sb-pacpackages",	0,	8},
 	/* {"",	"sb-news",		0,	6}, */
 	/* {"",	"sb-crypto",	0,	13}, */
 	/* {"",	"sb-price bat \"Basic Attention Token\" 🦁",	0,	20}, */
